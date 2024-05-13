2d761db7764d2e2b41cf7cb873c4871ae55b85d8
Khanyicou <136009825+Khanyicou@users.noreply.github.com>
2024-05-13 18:07:11 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,22 +4,26 @@
  * navbar toggle
  */
 
+// Selecting necessary elements
 const overlay = document.querySelector("[data-overlay]");
 const navbar = document.querySelector("[data-navbar]");
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 
+// Function to toggle navbar visibility
 const navToggleFunc = function () {
-  navToggleBtn.classList.toggle("active");
-  navbar.classList.toggle("active");
-  overlay.classList.toggle("active");
+  navToggleBtn.classList.toggle("active"); // Toggles active class for toggle button
+  navbar.classList.toggle("active"); // Toggles active class for navbar
+  overlay.classList.toggle("active"); // Toggles active class for overlay
 }
 
-navToggleBtn.addEventListener("click", navToggleFunc);
-overlay.addEventListener("click", navToggleFunc);
+// Event listeners for toggling navbar visibility
+navToggleBtn.addEventListener("click", navToggleFunc); // Click event for toggle button
+overlay.addEventListener("click", navToggleFunc); // Click event for overlay
 
+// Event listeners for closing navbar when a nav link is clicked
 for (let i = 0; i < navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", navToggleFunc);
+  navbarLinks[i].addEventListener("click", navToggleFunc); // Click event for each nav link
 }
 
 
@@ -28,9 +32,10 @@ for (let i = 0; i < navbarLinks.length; i++) {
  * header active on scroll
  */
 
+// Selecting header element
 const header = document.querySelector("[data-header]");
 
 window.addEventListener("scroll", function () {
-  window.scrollY >= 10 ? header.classList.add("active")
-    : header.classList.remove("active");
+  window.scrollY >= 10 ? header.classList.add("active") // Adds active class if scrolled more than 10px
+    : header.classList.remove("active"); // Removes active class if scrolled less than 10px
 });
